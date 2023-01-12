d7f380064784952ae38b40ac04748f87c8d8e05f
awbrenem <78624201+awbrenem@users.noreply.github.com>
2023-01-12 15:21:48 -0500

    minor updates

--- rockets/Endurance/end_polarization.py
+++ rockets/Endurance/end_polarization.py
@@ -2,8 +2,6 @@
 Determine polarization of various waves from Endurance.
 """
 
-
-
 import sys 
 sys.path.append('/Users/abrenema/Desktop/code/Aaron/github/mission_routines/rockets/Endurance/')
 sys.path.append('/Users/abrenema/Desktop/code/Aaron/github/plasma-physics-general/')
@@ -14,20 +12,10 @@ import matplotlib.pyplot as plt
 #import plasmapy
 from astropy import units as u  
 sys.path.append('/Users/abrenema/Desktop/code/Aaron/github/signal_analysis/')
+import filter_wave_frequency
 import plot_spectrogram as ps
 from scipy import signal
 
-
-
-from scipy.signal import butter, lfilter
-
-def butter_bandpass(lowcut, highcut, fs, order=5):
-    return butter(order, [lowcut, highcut], fs=fs, btype='band')
-
-def butter_bandpass_filter(data, lowcut, highcut, fs, order=5):
-    b, a = butter_bandpass(lowcut, highcut, fs, order=order)
-    y = lfilter(b, a, data)
-    return y
 
 
 
@@ -62,8 +50,8 @@ wf34 = evlf.dvlf34_mvm
 
 #Spectral overview
 fs = evlf.samplerate
-freq12, tspec12, power12 = signal.spectrogram(wf12, fs, nperseg=16384,noverlap=16384/2) #, return_onesided=1)
-freq34, tspec34, power34 = signal.spectrogram(wf34, fs, nperseg=16384,noverlap=16384/2) #, return_onesided=1)
+freq12, tspec12, power12 = signal.spectrogram(wf12, fs, nperseg=16384,noverlap=16384/2,window='hann') #, return_onesided=1)
+freq34, tspec34, power34 = signal.spectrogram(wf34, fs, nperseg=16384,noverlap=16384/2,window='hann') #, return_onesided=1)
 #ps.plot_spectrogram(tspec12,freq12,power12,vr=[-80,-40], xr=[900,1000],yr=[6000,10000], yscale='linear')
 #ps.plot_spectrogram(tspec12,freq12,power12,vr=[-80,-40],yr=[0,10000],xr=[100,900], yscale='linear')
 ps.plot_spectrogram(tspec12,freq12,power12,vr=[-80,-40],yr=[10,1000],xr=[100,900], yscale='log')
@@ -74,9 +62,9 @@ ps.plot_spectrogram(tspec12,freq12,power12,vr=[-80,-40],yr=[10,1000],xr=[100,900
 #Isolate waveform for polarization
 
 #tr = [400, 405]
-tr = [146, 146.00015]
-fr = [6100, 6300]
-tr = [470,00]
+#tr = [146, 146.00015]
+fr = [5500, 6000]
+tr = [130, 140]
 #fr = [5000, 5900]
 #fr = [6500, 6800]
 
@@ -117,14 +105,13 @@ ps.plot_spectrogram(tspec34,freq34,p34,vr=[-100,-40], xr=tr,yr=fr, yscale='log')
 
 
 #Bandpass filter waveform to preferred freqs and times
-filt12 = butter_bandpass_filter(wf12, fr[0],fr[1],fs,order=2)
-filt34 = butter_bandpass_filter(wf34, fr[0],fr[1],fs,order=2)
+filt12 = filter_wave_frequency.butter_bandpass_filter(wf12, fr[0],fr[1],fs,order=2)
+filt34 = filter_wave_frequency.butter_bandpass_filter(wf34, fr[0],fr[1],fs,order=2)
 
 #reduce to desired times only
 goodt = np.squeeze(np.where((tvals > tr[0]) & (tvals < tr[1])))
 filt12 = filt12[goodt]
 filt34 = filt34[goodt]
-
 
 #Plot hodograms 
 fig, axs = plt.subplots()
@@ -135,10 +122,8 @@ axs.set_xlim(-1*maxv, maxv)
 axs.set_ylim(-1*maxv, maxv)
 axs.set_aspect(1)
 
-
-
-
 print("here")
+
 
 
 
