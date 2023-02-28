ae9469ad67904b1671bb0031152199056ecec2ae
awbrenem <78624201+awbrenem@users.noreply.github.com>
2023-02-28 15:38:01 -0500

    first working version

--- rockets/Endurance/end_transfer_function.py
+++ rockets/Endurance/end_transfer_function.py
@@ -47,7 +47,7 @@ def end_load_gainphase(fn):
     #change to radians
     prad = [np.deg2rad(i) for i in p]
 
-    #change gain from dB to linear scaler for calculation of transfer function
+    #change gain from dB to linear scale for calculation of transfer function
     #From Steve Martin email on Nov 7, 2022: 
     #Gain=10^(0.05 * (opchan+gainoffset))
     offset = 0.
@@ -65,7 +65,7 @@ def end_load_gainphase(fn):
     axs[0].set_yscale('linear')
     axs[1].set_ylim(0,20)
     axs[0].set(ylabel='gain(dB)',xlabel='freq(kHz)')
-    axs[1].set(ylabel='gain',xlabel='freq(kHz)')
+    axs[1].set(ylabel='gain(linear)',xlabel='freq(kHz)')
     axs[2].set(ylabel='phase(deg)',xlabel='freq(kHz)')
     #axs[:].set_xlim(-40,10)
     plt.show()
@@ -83,6 +83,13 @@ def end_load_gainphase(fn):
  
 prad, Hmag, f = end_load_gainphase("Endurance_Analog 1_VLF12D_6-30000-100.txt")
 
+
+#Chop off values above 15 kHz, the Nyquist freq. The calibration data goes out to 30 kHz. 
+#Not doing this can mess up the interpolation b/t lowres and highres data
+
+f = f[:91]
+prad = prad[:91]
+Hmag = Hmag[:91]
 
 
 
@@ -109,162 +116,148 @@ yr = [0,12000]
 #FFT to get power spectral density (V^2/Hz). 
 #Mode defaults to PSD (V^2/Hz). 
 #Complex consists of amplitude (where magnitude = abs(complex))
-fspec, tspec, powerc = signal.spectrogram(vlf12, fs, nperseg=1024,noverlap=1024/2,window='hann',return_onesided=True,mode='complex')
 fspec, tspec, powerm = signal.spectrogram(vlf12, fs, nperseg=1024,noverlap=1024/2,window='hann',return_onesided=True,mode='magnitude')
-fspec, tspec, powera = signal.spectrogram(vlf12, fs, nperseg=1024,noverlap=1024/2,window='hann',return_onesided=True,mode='angle')
+fspec, tspec, powerc = signal.spectrogram(vlf12, fs, nperseg=1024,noverlap=1024/2,window='hann',return_onesided=True,mode='complex')
+#fspec, tspec, powera = signal.spectrogram(vlf12, fs, nperseg=1024,noverlap=1024/2,window='hann',return_onesided=True,mode='angle')
 #ft, tt, pt = signal.spectrogram(vlf12, fs, nperseg=1024,noverlap=1024/2,window='hann',return_onesided=True)
 
+#-------------------------------------------------------------
+#FFT entire Endurance waveform to apply transfer function
+#-------------------------------------------------------------
 
 
+from scipy.fft import rfft, irfft
+
+
+vlf12FFT = rfft(vlf12)
+power12FFT = vlf12FFT**2
+
+
+
+N = len(vlf12FFT)
+n = np.arange(N)
+T = N/fs
+freq = n/T/2 
+
+fig, axs = plt.subplots(3)
+axs[0].plot(freq,np.abs(power12FFT))
+axs[1].plot(freq,np.real(power12FFT))
+axs[2].plot(freq,np.imag(power12FFT))
+for i in range(3): axs[i].set_xscale('linear')
+for i in range(3): axs[i].set_xlim(0,100)
+
+
+
+print('here')
 
 #------------------------------------------------------------------
-#Transfer function  H(w) = |H|*exp(i*theta)
+#Transfer function H(w) = |H|*exp(i*theta)
 #------------------------------------------------------------------
 
 
 #Interpolate transfer function to frequencies of FFT'd waveform data
 interp = interp1d(f,Hmag,kind='cubic', bounds_error=False)
-Hmag = interp(fspec)
+Hmag2 = interp(freq)
 interp2 = interp1d(f,prad,kind='cubic', bounds_error=False)
-prad = interp2(fspec)
+prad2 = interp2(freq)
 
-H = [Hmag[i] * np.exp(1j*prad[i]) for i in range(len(prad))]
+#********
+#NoTE: Two issues to deal with here before we define the transfer function:
+# 1) the gains at the lowest freqs (<10 Hz) are artificial and come from the calibration procedure. 
+#   If I leave these as they are (gains < 1) then this ends up hugely boosting calibrated data resulting in a 10 Hz continuous signal that isn't real. 
+#   I need to just get rid of this. Set any gain <30 Hz to the value of the gain at 100 Hz (~17.3) 
+# 2) The interpolated phase has an over/undershoot at 1 kHz. This is due to interpolating over a sudden change.
+#   Need to unwrap values, interpolate, then rewrap
+#********
 
+#Remove artifical gains below about 30 Hz. 
+badgain_ind = list(map(tuple, np.where(freq < 30)))
+Hmag2[badgain_ind] = 17.6
 
-"""
-fig, axs = plt.subplots(2)
-axs[0].plot(f,p)
-axs[1].plot(fspec, prad)
-axs[0].set_xlim(0,15000)
-axs[1].set_xlim(0,15000)
-plt.show()
-"""
-
-#----------------------------------------------------------------------------
-#Compare distribution of uncorrected vs corrected power FOR A SINGLE TEST TIME
-#----------------------------------------------------------------------------
-
-
-#Apply transfer function to the complex FFT data.
-#(1) apply it to the complex FFT 
-ttime = 2000
-
-
-
-#The transfer function correction applies to the POWER, not the amplitude
-corrected = powerc[:,ttime]/H
-#corrected = powerc[:,ttime]/Htst
-
-#Check to make sure the gain has been applied correctly
-ratio = [np.abs(powerc[i,ttime])/np.abs(corrected[i]) for i in range(len(H))]
 
 
 fig, axs = plt.subplots(2)
-axs[0].plot(fspec,np.abs(powerc[:,ttime]),'.',fspec,np.abs(corrected),'x')
-axs[1].plot(fspec,ratio)
-axs[0].set_xscale('log')
-axs[0].set_yscale('log')
-axs[0].set_xlim(1,100000)
-axs[1].set_xscale('log')
-axs[1].set_yscale('log')
-axs[1].set_xlim(1,100000)
-axs[0].set_ylim(1e-7,1e-3)
-plt.show()
+axs[0].plot(f, Hmag, freq, Hmag2)
+axs[1].plot(f, prad, freq, prad2)
+for i in range(2): axs[i].set_xscale('log')
+for i in range(2): axs[i].set_xlim(1,20000)
+axs[1].set_ylim(-4,4)
 
 
-#----------------------------------------------------------------------------
-#Gain/phase correct all the spectral data. 
-#----------------------------------------------------------------------------
+H = [Hmag2[i] * np.exp(1j*prad2[i]) for i in range(len(prad2))]
 
-powerc_corr = powerc.copy()
-for i in range(len(powerc[0,:])):   #Loop through each time
-    powerc_corr[:,i] = powerc[:,i]/H
+fig2, axs2 = plt.subplots(3)
+axs2[0].plot(freq,np.abs(H))
+axs2[1].plot(freq,np.real(H))
+axs2[2].plot(freq,np.imag(H))
+for i in range(3): axs2[i].set_xscale('log')
 
 
 
 
-ptmp = np.abs(powerc_corr)
+#Apply transfer function to FFT'd data (amplitude, not power)
+vlf12FFTc = [vlf12FFT[i]/H[i] for i in range(len(H))]
 
-vr = [-80,-20]
-xr = [100,500]
-yr = [0,12000]
-
-fig, axs = plt.subplots(2)
-ps.plot_spectrogram(tspec, fspec, powerm, vr=vr, yscale='linear', zscale='log', xr=xr, yr=yr, ax=axs[0], show=False)
-axs[0].set_title = 'test'
-ps.plot_spectrogram(tspec, fspec, ptmp, vr=vr, yscale='linear', zscale='log', xr=xr, yr=yr, ax=axs[1])
-axs[1].set_title = 'test2'
-plt.show()
+freq2 = list(freq)
 
 
-plt.plot(fspec,powerm[:,3000])
-plt.plot(fspec,powerc[:,3000])
-plt.plot(fspec,powerc_corr[:,3000])
 
 
-#------------------------------------------------------
-#invert the corrected spectral data to waveforms
-#NOTE: IT SEEMS THAT I NEED THE NEGATIVE FREQUENCIES TO USE IFFT
-#------------------------------------------------------
+v1 = np.abs(vlf12FFT)
+v2 = np.abs(vlf12FFTc)
+rat = [v1[i]/v2[i] for i in range(len(v1))]
+
+fig3,axs3 = plt.subplots(4)
+axs3[0].plot(freq2,np.abs(vlf12FFT))
+axs3[1].plot(freq2,np.abs(vlf12FFTc))
+axs3[2].plot(freq2,rat)
+axs3[2].set_yscale('linear')
+axs3[3].plot(freq2,np.abs(H))
+axs3[3].set_yscale('linear')
+for i in range(4): axs3[i].set_xlim(0,50)
 
 
-#array with positive followed by negative frequencies (as required by ifft)
-fspecc = np.concatenate((fspec,-1*np.flip(fspec)))
-
-#Loop through each time and add the negative freq data to the positive freq data to create array with both
-pgoo = []
-pgoo_corr = []
-for i in range(len(tspec)):
-    ftst = np.concatenate((powerc[:,i],np.flip(powerc[:,i])))
-    pgoo.append(ftst)
-    ftst = np.concatenate((powerc_corr[:,i],np.flip(powerc_corr[:,i])))
-    pgoo_corr.append(ftst)
-
-powerc2 = np.transpose(pgoo)
-powerc_corr2 = np.transpose(pgoo_corr)
 
 
-#plt.plot(fspecc,pgoo[:,100])
-plt.plot(fspecc,powerc2[:,0])
-plt.plot(fspecc,powerc_corr2[:,0])
-plt.ylim(-0.01,0.01)
-plt.xlim(-1000,1000)
+#Before I inverse transform I need to get rid of NaN values at the beginning of new data arrays
+#good = list(map(tuple, np.where(np.isfinite(vlf12FFTc))))
+bad = list(map(tuple, np.where(np.isnan(vlf12FFTc))))
+
+vlf12FFTc = np.asarray(vlf12FFTc)
+vlf12FFTc[bad] = 0
 
 
-#get rid of NaN values
-#ttime = 2000
-#ptmp = powerc[5:,ttime]
-#ptmp_corr = powerc_corr[5:,ttime]
 
 
-wf = np.fft.ifft(powerc2[:,10])
-wf_corr = np.fft.ifft(powerc_corr2[:,5900])
+#Inverse FFT to get back to corrected waveform
+wf = irfft(vlf12FFT)
+wf_corr = irfft(vlf12FFTc)
 
-wf2 = np.ndarray.flatten(wf)
-wf_corr2 = np.ndarray.flatten(wf_corr)
 
-wf2 = wf2[:len(wf2)//2]
-wf_corr2 = wf_corr2[:len(wf_corr2)//2]
+
+fig,axs = plt.subplots(3)
+axs[0].plot(tvlf,vlf12)
+axs[1].plot(tvlf,wf)
+for i in range(3): axs[i].set_ylim([-0.5,0.5])
+axs[2].plot(tvlf,wf_corr)
+axs[2].set_ylim(-0.02,0.02)
+for i in range(3): axs[i].set_xlim([393.1+0.0625,393.1+0.065])
+
+
+#Phase shift test using normalized gain versions.
+goomax = np.max(wf)
+wfnorm = wf/goomax
+goomax = np.max(wf_corr)
+wf_corrnorm = wf_corr/goomax
+
+plt.plot(tvlf,wfnorm, tvlf,wf_corrnorm)
+plt.xlim([393.1+0.0625,393.1+0.065])
+plt.ylim(-0.02,0.02)
 
 
 
 
 
-wf = np.fft.ifft(powerc2)
-wf_corr = np.fft.ifft(powerc_corr2)
-
-wf2 = np.ndarray.flatten(wf)
-wf_corr2 = np.ndarray.flatten(wf_corr)
-
-wf2 = wf2[:len(wf2)//2]
-wf_corr2 = wf_corr2[:len(wf_corr2)//2]
-
-#wf = np.fft.ifft(np.abs(powerc[:,ttime]))
-plt.plot(np.real(wf2[100:200]))
-plt.plot(np.real(wf_corr2[100:200]))
-
-plt.plot(np.abs(wf_corr2[0:len(wf_corr2)//10]))
-plt.show()
 
 
 """
